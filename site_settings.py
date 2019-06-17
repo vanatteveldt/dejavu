f19f176efc4bd9712fc4f82cc43e7a1abdb80d5f
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-06-17 14:10:17 +0200

    Remove site settings from git

--- site_settings.py
+++
@@ -1,7 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amcat.vu@gmail.com'
-EMAIL_HOST_PASSWORD = 'mmdqovjzibvxpgty'
-EMAIL_USE_TLS = True
-
